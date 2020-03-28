742518b7f2402c9c210115036a16d1149582185a
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-03-28 22:12:01 +0200

    update exercise 1 still trying

--- Week1/exercise-1/connectToSQL.js
+++ Week1/exercise-1/connectToSQL.js
@@ -17,33 +17,58 @@ con.connect(function (err) {
     con.query("CREATE DATABASE IF NOT EXISTS meetup", function (err, result) {
         if (err) throw err;
         console.log("Database created");
+        afterDbCreation();
     })
 
 });
+
+
 
 //Create a table in the meetup database---------------------------------------------------------------------------------
+function afterDbCreation() {
 
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "hyfuser",
-    password: "hyfpassword",
-    database: "meetup"
-});
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "hyfuser",
+        password: "hyfpassword",
+        database: "meetup"
+    });
 
-con.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected!");
+    con.connect(function (err) {
+        if (err) throw err;
+        console.log("Connected!");
 
-    con.query("CREATE TABLE IF NOT EXISTS Invitee  (invitee_no INT, invitee_name CHAR, invited_by CHAR)", function (err, result) {
-        if (err) throw err;
-        console.log("Table created");
-    })
-    con.query("CREATE TABLE IF NOT EXISTS Room  (room_no INT, room_name CHAR, floor_number INT)", function (err, result) {
-        if (err) throw err;
-        console.log("Table created");
-    })
-    con.query("CREATE TABLE IF NOT EXISTS Meeting  (meeting_no INT, meeting_title CHAR, starting_time TIME, ending_time TIME, room_no INT)", function (err, result) {
-        if (err) throw err;
-        console.log("Table created");
-    })
-});
+        con.query("CREATE TABLE IF NOT EXISTS Invitee  (invitee_no INT, invitee_name LONGTEXT, invited_by LONGTEXT)", function (err, result) {
+            if (err) throw err;
+            console.log("Table created");
+        })
+        con.query("CREATE TABLE IF NOT EXISTS Room  (room_no INT, room_name LONGTEXT, floor_number INT)", function (err, result) {
+            if (err) throw err;
+            console.log("Table created");
+        })
+        con.query("CREATE TABLE IF NOT EXISTS Meeting  (meeting_no INT, meeting_title LONGTEXT, starting_time TIME, ending_time TIME, room_no INT)", function (err, result) {
+            if (err) throw err;
+            console.log("Table created");
+        })
+
+
+        //query to insert data into tables-----------------------------------------------------------------------------------------------------
+
+        for (let i = 0; i < 5; i++) {
+            con.query("INSERT INTO Invitee VALUES (5, 'test', 'someone')", function (err, result) {
+                if (err) throw err;
+                console.log("Record inserted");
+            });
+            con.query("INSERT INTO Room VALUES (5, 'Highway', 1)", function (err, result) {
+                if (err) throw err;
+                console.log("Record inserted");
+            });
+
+            con.query("INSERT INTO Meeting VALUES (5, 'meeting title', '14:30','15:30' )", function (err, result) {
+                if (err) throw err;
+                console.log("Record inserted");
+            });
+        }
+    });
+
+}
