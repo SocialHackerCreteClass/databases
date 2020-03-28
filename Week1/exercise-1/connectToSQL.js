095fa5fff750cce0ad496738ee9f01531d050d6e
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-03-28 19:13:12 +0200

    Update connectToSQL.js

--- Week1/exercise-1/connectToSQL.js
+++ Week1/exercise-1/connectToSQL.js
@@ -1,14 +1,15 @@
 var mysql = require('mysql');
 
+
+//Create a database with con.connect query above-----------------------------------------------------------------
+
 var con = mysql.createConnection({
     host: "localhost",
     user: "hyfuser",
     password: "hyfpassword",
-    //after i create the database i enter the name here in order to add some tables to it!
-
 });
 
-//create a database called "meetup"
+
 
 con.connect(function (err) {
     if (err) throw err;
@@ -16,25 +17,33 @@ con.connect(function (err) {
     con.query("CREATE DATABASE IF NOT EXISTS meetup", function (err, result) {
         if (err) throw err;
         console.log("Database created");
-    });
+    })
+
 });
 
+//Create a table in the meetup database---------------------------------------------------------------------------------
 
-//after i create the database i enter the name here in order to add some tables to it!
 var con = mysql.createConnection({
     host: "localhost",
     user: "hyfuser",
     password: "hyfpassword",
     database: "meetup"
-
 });
 
 con.connect(function (err) {
     if (err) throw err;
     console.log("Connected!");
-    var sql = "CREATE TABLE Invitee (invitee_no VARCHAR(255), invitee_name VARCHAR(255), invited_by(VARCHAR))";
-    con.query(sql, function (err, result) {
+
+    con.query("CREATE TABLE IF NOT EXISTS Invitee  (invitee_no INT, invitee_name CHAR, invited_by CHAR)", function (err, result) {
         if (err) throw err;
         console.log("Table created");
-    });
+    })
+    con.query("CREATE TABLE IF NOT EXISTS Room  (room_no INT, room_name CHAR, floor_number INT)", function (err, result) {
+        if (err) throw err;
+        console.log("Table created");
+    })
+    con.query("CREATE TABLE IF NOT EXISTS Meeting  (meeting_no INT, meeting_title CHAR, starting_time TIME, ending_time TIME, room_no INT)", function (err, result) {
+        if (err) throw err;
+        console.log("Table created");
+    })
 });
